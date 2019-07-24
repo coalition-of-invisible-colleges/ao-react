b9d5271884d639ceefd63d027c86423bd45e76ee
taylor <rhodes4ao@gmail.com>
2019-07-23 23:03:50 -0700

    activered

--- src/components/Members/Row.vue
+++ src/components/Members/Row.vue
@@ -5,10 +5,10 @@
         .four.grid
             img(v-if='isLoggedIn', src='../../assets/images/loggedIn.svg')
             img(v-else, src='../../assets/images/loggedOut.svg')
-            label {{ m.name }}
+            label(:class='{redtx: m.active < 0}') {{ m.name }}
                 br
                 span(v-for='g in rowsGuilds')
-                    router-link.yellowtx(:to='"/task/" + g.taskId') {{ g.guild }} - 
+                    router-link.yellowtx(:to='"/task/" + g.taskId') {{ g.guild }} -
         .two.grid(v-if='!hasAnyVouches')
             img.btn.goldengun(v-if='!hasAnyVouches' src='../../assets/gifs/golden_gun.gif' @click='purgeAccount')
         .one.grid
