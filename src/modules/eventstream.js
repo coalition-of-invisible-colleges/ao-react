48513acd420880ef97c643865f4d2fa463bf9228
Anders Aamodt <anders.aamodt@gmail.com>
2019-08-05 21:24:20 -0700

    improved dogebubbles

--- src/modules/eventstream.js
+++ src/modules/eventstream.js
@@ -7,6 +7,22 @@ let memes = [
     'very happen',
 ]
 
+let colors = [
+    'white',
+    'red',
+    'maroon',
+    'yellow',
+    'olive',
+    'lime',
+    'green',
+    'aqua',
+    'teal',
+    'blue',
+    'navy',
+    'fuchsia',
+    'purple',
+]
+
 function bestMeme(){
   return memes[Math.floor(Math.random()*memes.length)];
 }
@@ -19,13 +35,19 @@ const mutations = {
             meme: bestMeme(),
             type: "",
             showEvent: false,
-            randomX: '45%'
+            randomX: '45%',
+            randomColors: [],
+            randomXs: [],
+            randomYs: []
         }
 
         newBubble.meme = bestMeme()
         newBubble.type = ev.type
         newBubble.showEvent = true
         newBubble.randomX = Math.random().toFixed(2) * 92 + '%'
+        for(let i = 0; i < 2; i++) newBubble.randomColors.push(colors[Math.floor(Math.random() * colors.length)])
+        for(let i = 0; i < 2; i++) newBubble.randomXs.push(Math.floor(Math.random() * 100) + '%')
+        for(let i = 0; i < 2; i++) newBubble.randomYs.push(Math.floor(Math.random() * 100) + '%')
         //newBubble.wiggleFactor = Math.random().toFixed(2) * 10
 
         state.push(newBubble)
