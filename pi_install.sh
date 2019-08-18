e0fc967035a270de24dac582c1e0e583ee10ca0d
Anders Aamodt <anders.aamodt@gmail.com>
2019-08-17 21:21:44 -0700

    improved tor install

--- pi_install.sh
+++ pi_install.sh
@@ -153,11 +153,26 @@ if [ $(tor --version  2>/dev/null | grep -c "0\.4\.0\.5") -eq 1 ];
 then
 	echo tor v0.4.0.5 already installed
 else
+	if [ $(dpkg-query -W -f='${Status}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ];
+	then
+		sudo apt install -y build-essential
+	fi
+
+	if [ $(dpkg-query -W -f='${Status}' fakeroot 2>/dev/null | grep -c "ok installed") -eq 0 ];
+	then
+		sudo apt install -y fakeroot
+	fi
+
+	if [ $(dpkg-query -W -f='${Status}' devscripts 2>/dev/null | grep -c "ok installed") -eq 0 ];
+	then
+		sudo apt install -y devscripts
+	fi
+
 	cd ~
 	wget https://dist.torproject.org/tor-0.4.0.5.tar.gz
 	tor=true
 	tar xf tor-0.4.0.5.tar.gz
-	cd tor
+	cd tor-0.4.0.5
 	./configure
 	make
 	sudo make install
@@ -238,7 +253,7 @@ fi
 if [ "$tor" = true ];
 then
 	rm -rf tor-*
-	tor-0.4.0.5.tar.gz
+	rm tor-0.4.0.5.tar.gz
 fi
 
 echo the AO is installed
