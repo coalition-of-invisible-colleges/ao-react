2cb8e2902055574bc733f420ebf2f82fc05da551
taylor <rhodes4ao@gmail.com>
2019-11-13 15:18:26 -0800

    add f

--- src/components/Resources/Row.vue
+++ src/components/Resources/Row.vue
@@ -1,53 +1,36 @@
 <template lang='pug'>
 
-.resources(@dblclick='goIn')
-    h3 {{ r.name }}
-        span(v-if='r.stock && r.stock >= 0').stock ({{ r.stock }} remain)
-    img.smallguild(src='../../assets/images/treasurechestnobkgrndwhiteD.png')
-    span {{ val }}
-    .invoices(v-if='showInvoices')
-        pay-req(v-if='invoice', :i='invoice')
-    .row
-        .seven.columns.recent
-            label recently used broken, see sun-badge
-        .five.columns
-            button.use()
-                button(@click='use("A")') A
-                button(@click='use("B")') B
-                button(@click='use("C")') C
-                button(@click='use("D")') D
-                button(@click='use("E")') E
+.memberrow.membershipcard(@dblclick='goIn')
+    .row.center
+        label.hackername {{ r.name }}
+            span
+                img.smallguild(src='../../assets/images/treasurechestnobkgrndwhiteD.png')
+                label.stash(v-if='card.boost') {{ card.boost.toFixed(2) }}
+                label.stash(v-else) 0
+        button(@click='use("A")') A
+        button(@click='use("B")') B
+        button(@click='use("C")') C
+        button(@click='use("D")') D
+        button(@click='use("E")') E
+        button(@click='use("E")') F
+    .bottomleft(v-if='card.boost')
+
+    .bottomright
+    .clearboth
 </template>
 
 <script>
-import Current from './Current'
-import PayReq from './PayReq'
-import calculations from '../../calculations'
-import SoundFX from '../../utils/sounds'
+
+import Addr from '../Members/Addr'
+import PreviewDeck from '../Deck/PreviewDeck'
+import Vouch from '../Members/Vouch'
 
 export default {
-    data(){
-        return { showInvoices: false, notes: 'A'  }
-    },
-    props: ['r', 'c'],
-    components: { Current, PayReq },
-    computed: {
-        val(){
-            return calculations.calculateTaskPayout(this.resourceCard)
-        },
-        resourceCard(){
+    props: ['r'],
+    components: { Addr, PreviewDeck, Vouch},
+    computed:{
+        card(){
             return this.$store.getters.hashMap[this.r.resourceId]
-        },
-        trackStock(){
-            return !(this.r.stock == undefined)
-        },
-        currentMembers(){
-            let currentMembers = []
-            return currentMembers
-        },
-        sats(){
-            let sats = this.r.charged / this.$store.state.cash.spot * 100000000
-            return parseInt( Math.round(sats).toFixed(0) )
         },
     },
     methods: {
@@ -68,14 +51,47 @@ export default {
             let top = this.c.indexOf(this.r.resourceId)
             console.log("goIn called with TOP: ", top)
             if (top > -1){
-                this.$router.push('/' + this.$store.state.upgrades.mode)
                 this.$store.dispatch("goIn", {
                     parents: [],
                     panel: this.c,
                     top,
                 })
+                this.$router.push('/' + this.$store.state.upgrades.mode)
             }
         },
+        getName(taskId){
+            let name
+            this.$store.state.tasks.some(t => {
+                if (taskId === t.taskId){
+                    name = t.name
+                    return true
+                }
+            })
+            return name
+        },
+        toggleActivated() {
+            if(this.m.memberId !== this.$store.getters.member.memberId) {
+                return
+            }
+            if(this.$store.getters.member.active > 0) {
+                this.deactivate()
+            } else {
+                this.activate()
+            }
+        },
+        deactivate() {
+            this.$store.dispatch("makeEvent", {
+                type: 'member-deactivated',
+                memberId: this.$store.getters.member.memberId,
+            })
+        },
+        activate() {
+            this.$store.dispatch("makeEvent", {
+                type: 'member-activated',
+                memberId: this.$store.getters.member.memberId,
+            })
+        }
+
     }
 }
 
@@ -83,62 +99,137 @@ export default {
 
 <style lang="stylus" scoped>
 
-@import '../../styles/button'
-@import '../../styles/skeleton'
-@import '../../styles/skeleton-button'
 @import '../../styles/colours'
-
-img
-    height: 1.5em
-    z-index: 100
+@import '../../styles/skeleton'
+@import '../../styles/grid'
+@import '../../styles/tooltips'
+@import '../../styles/button'
 
 button
-    color: white
-    button
-        width: 20%
-.resources
-    color: accent1
-    background-color: darkteal
-    border-color: accent4
-    border-bottom-style: solid
-    border-width: 3px
+    width: 4em
+    height:2em
+    margin: 1.1em
+    font-size: 1.5em
+
+img
+    height: 2em
+
+label
+    font-size: 1.246em
+    font-weight: normal;
+    margin: 1em
+    display: block
+
+.hackername
+    font-family: monospace
+    font-size: 1.5em
+
+.membershipcard
+    padding: 1em
+    background: rgba(22, 22, 22, 0.2)
+    text-align: center
+
+.agedwrapper
+    position: relative
+
+.agedbackground
+    background-image: url('../../assets/images/paper.jpg')
+    background-repeat: no-repeat
+    background-position: center center
+    background-size: cover
+    top: 0
+    left: 0
+    bottom: 0
+    right: 0
+    position: absolute
     width: 100%
-    margin-bottom: 1em
-    padding-bottom: 1em
+    height: 100%
+    pointer-events: none
+    //border-radius: 12px
+    z-index: -1
 
-.stock
-    color: accent2
+.freshpaper
+    background-image: url('../../assets/images/paper.jpg')
+    opacity: 0.3
 
-.refill
-    border-color: green
-    background: green
+.weekoldpaper
+    background-image: url('../../assets/images/paper_aged_1.png')
+    opacity: 0.3
 
-.recent
-    font-size: .888em
+.montholdpaper
+    background-image: url('../../assets/images/paper_aged_2.png')
+    opacity: 0.3
 
-.pay
-    border-color: accent2
-    background-color: accent2
+.threemontholdpaper
+    background-image: url('../../assets/images/paper_aged_3.png')
+    opacity: 0.3
 
-.use
-    border-color: lightteal
-    background-color: lightteal
-
-.book
-    border-color: purple
-    background-color: purple
-
-.current
-    background: lightGrey
-    color: main
-    padding: 0.3em
-    border-radius: 0.5em
-
-.fw
+.smallcaps
+    color: #fff
     width: 100%
+    border-radius: 50%
+    opacity: 0.75
+    padding: 0.5em
+    border-style: solid
+    border-color: white
+    border-width: 2px
 
-.viney
+.smallguild
+    height: 2em
+
+.bottomleft, .bottomright
+    width: fit-content
+    position: relative
+    bottom: 0
+
+.bottomleft
+    float: left
+    left: 0
+
+.bottomright
+    right: 0
     float: right
-    height: 1.3em
 
+.stash
+    display: inline
+    margin-left: 0.5em
+    position: relative
+    top: -0.35em
+
+.clearboth
+    clear: both
+
+.gui
+    font-size: 1.7em
+    cursor: pointer
+
+.title
+    text-align: center
+    font-size: 1.8em
+    margin-top: 0.5em
+    font-weight: bold
+
+.help
+    font-size: 1.3em
+
+.suggest
+    font-style: italic
+    font-size: 1.3em
+
+.dogecoin
+    width: 3em
+    height: 3em
+    cursor: pointer
+
+.faded
+    opacity: 0.39
+
+.tooltiptext.membertooltip
+    width: 20em
+    z-index: 151
+    left: 7em
+    top: -11em
+
+ul.left
+    text-align: left
 </style>
