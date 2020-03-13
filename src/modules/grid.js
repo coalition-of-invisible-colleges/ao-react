4c3968d469828fb76d367c0f87bf1e4f74041395
xiphiness <xiphiness@protonmail.com>
2020-03-13 01:19:42 -0400

    grid modules

---
+++ src/modules/grid.js
@@ -0,0 +1,41 @@
+const _ = require("lodash");
+const M = require("../mutations");
+
+const state = [];
+
+const mutations = {
+  setCurrent(grid, current) {
+    _.keys(grid).forEach(x => {
+      _.keys(grid[x]).forEach(y => {
+        delete grid[x][y];
+      });
+      delete grid[x];
+    });
+    _.assign(grid, current.grid);
+  },
+  applyEvent: M.tasksMuts
+};
+
+const actions = {
+  addGrid() {
+    request
+      .post("/events")
+      .set("Authorization", this.$store.state.loader.token)
+      .send({
+        type: "task-created",
+        name: potentialCard,
+        color: this.task.color,
+        deck: [this.$store.getters.member.memberId],
+        inId: this.taskId
+      })
+      .end((err, res) => {
+        if (err) return console.log(err);
+      });
+  }
+};
+
+module.exports = {
+  state,
+  mutations,
+  actions
+};
