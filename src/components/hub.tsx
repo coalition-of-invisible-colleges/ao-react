2d615835bc81ac8bf3b40f70e8d411c400b3b7f3
Anders Aamodt <anders.aamodt@gmail.com>
2020-06-30 05:00:57 -0700

    added community hub card

--- src/components/hub.tsx
+++ src/components/hub.tsx
@@ -4,6 +4,8 @@ import aoStore from '../client/store'
 import AoSourceStack from './sourceStack'
 import Sun from '../assets/images/sun.svg'
 import AoGrid from './grid'
+import AoSmartCard from './smartCard'
+import api from '../client/api'
 
 interface State {
   hubPanel: boolean
@@ -19,11 +21,17 @@ export default class AoHub extends React.Component<{}, State> {
     super(props)
     this.state = defaultState
     this.toggleHubPanel = this.toggleHubPanel.bind(this)
+    this.addCommunityCard = this.addCommunityCard.bind(this)
     this.renderHubButton = this.renderHubButton.bind(this)
   }
 
   toggleHubPanel() {
     this.setState({ hubPanel: !this.state.hubPanel })
+  }
+
+  addCommunityCard() {
+    api.createCard('community hub')
+    console.log('card added')
   }
 
   renderHubButton() {
@@ -71,11 +79,23 @@ export default class AoHub extends React.Component<{}, State> {
     console.log('top missions is ', topMissions, ' and top cards is ', topCards)
 
     // <AoGrid taskId={aoStore.state.tasks[0].taskId} />
+    let communityCard = aoStore.cardByName.get('community hub')
+
+    console.log('communityCard is ', communityCard)
 
     return (
       <div id={'hub'} className={'open'}>
         {this.renderHubButton()}
         <div className={'popover'}>
+          <div className={'left'}>
+            {communityCard && communityCard.hasOwnProperty('taskId') ? (
+              <AoSmartCard taskId={communityCard.taskId} cardStyle={'full'} />
+            ) : (
+              <p onClick={this.addCommunityCard} className={'action'}>
+                +H.U.B.
+              </p>
+            )}
+          </div>
           <div className={'right'}>
             <h2>Top Missions</h2>
             <AoSourceStack cards={topMissions} alwaysShowAll={true} />
