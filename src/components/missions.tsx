c6160e6c9cabfda2cf7bb31af305179baa137d66
Anders Aamodt <anders.aamodt@gmail.com>
2020-07-11 16:35:13 -0700

    only top-level missions show in Missions Index now

--- src/components/missions.tsx
+++ src/components/missions.tsx
@@ -31,11 +31,49 @@ export default class AoMissions extends React.Component<{}, State> {
     let missions = aoStore.state.tasks.filter(task => {
       return task.hasOwnProperty('guild') && task.guild.length >= 1
     })
-    missions.reverse()
-
     if (missions.length < 1) {
       return ''
     }
+
+    let getProjectCards = card => {
+      let projectCards: Task[] = []
+      let allSubCards = card.priorities.concat(card.subTasks, card.completed)
+
+      allSubCards.forEach(tId => {
+        let subCard = aoStore.hashMap.get(tId)
+        if (subCard) {
+          if (subCard.guild && subCard.guild.length >= 1) {
+            projectCards.push(subCard)
+          }
+        }
+      })
+
+      if (card.grid && card.grid.rows) {
+        Object.entries(card.grid.rows).forEach(([y, row]) => {
+          Object.entries(row).forEach(([x, cell]) => {
+            let gridCard = aoStore.hashMap.get(cell)
+            if (gridCard.guild && gridCard.guild.length >= 1) {
+              projectCards.push(gridCard)
+            }
+          })
+        })
+      }
+
+      return projectCards
+    }
+
+    let projectCards = []
+    projectCards = projectCards.concat(
+      ...missions.map(task => getProjectCards(task))
+    )
+    let subProjectCards = []
+    subProjectCards = subProjectCards.concat(
+      ...projectCards.map(task => getProjectCards(task))
+    )
+
+    missions = missions.filter(task => {
+      return !projectCards.includes(task) && !subProjectCards.includes(task)
+    })
 
     return (
       <div className={'results'}>
