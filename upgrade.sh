a710a022605a84ff19e5040b2f76f1c51bb86737
Anders <anders.aamodt@gmail.com>
2020-11-20 23:29:38 -0800

    added docker management to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,15 @@
 # Updates the AO to the newest version
 # To run, first `chmod u+x upgrade.sh` then `./upgrade.sh`
+# Automatically shuts down your jitsi server first to speed up compiling
+# Does `npm install` first to make sure that build succeeds
 
 # Todo: Add an switch to create the swap file automatically
+
+if [ -d "$HOME/docker-jitsi-meet" ]; then
+	cd ~/docker-jitsi-meet
+	docker-compose down
+	cd -
+fi
 
 # On low memory systems, linking fails without this
 export NODE_OPTIONS=--max-old-space-size=8192
@@ -9,5 +17,14 @@ export NODE_OPTIONS=--max-old-space-size=8192
 # Pull the newest version of the AO
 git pull
 
+# Install
+npm install
+
 # Compile
 npm run webpack
+
+if [ -d "$HOME/docker-jitsi-meet" ]; then
+	cd ~/docker-jitsi-meet
+	docker-compose up -d
+	cd -
+fi
