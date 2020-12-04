8bb3c6040f84e1284f1aeaef924f90f286e9da8f
Anders <anders.aamodt@gmail.com>
2020-12-03 21:56:08 -0800

    bad code

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -3,13 +3,13 @@ import aoStore from '../client/store'
 import api from '../client/api'
 import { Redirect } from 'react-router-dom'
 
-export default async function AoMember() {
+export default function AoMember() {
 	const [hubId, setHubId] = React.useState()
 
 	React.useEffect(() => {
 		let communityCard = aoStore.cardByName.get('community hub')
 		if (!communityCard) {
-			return await api.createCard('community hub').then(result => {
+			api.createCard('community hub').then(result => {
 				const newTaskId = JSON.parse(res.text).event.taskId
 				setHubId(newTaskId)
 			})
