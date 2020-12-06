1100e5f5d5873af322be23265fa2393091341e13
Anders <anders.aamodt@gmail.com>
2020-12-06 01:09:03 -0800

    added gpio export script for pi

---
+++ install/export_gpio.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+echo 2 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio2/direction
+echo 1 > /sys/class/gpio/gpio2/value
+
+echo 3 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio3/direction
+echo 1 > /sys/class/gpio/gpio3/value
+
+echo 4 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio4/direction
+echo 1 > /sys/class/gpio/gpio4/value
+
+echo 5 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio5/direction
+echo 1 > /sys/class/gpio/gpio5/value
+
+echo 6 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio6/direction
+echo 1 > /sys/class/gpio/gpio6/value
+
+echo 7 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio7/direction
+echo 1 > /sys/class/gpio/gpio7/value
+
+echo 8 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio8/direction
+echo 1 > /sys/class/gpio/gpio8/value
+
+echo 9 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio9/direction
+echo 1 > /sys/class/gpio/gpio9/value
+
+echo 10 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio10/direction
+echo 1 > /sys/class/gpio/gpio10/value
+
+echo 11 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio11/direction
+echo 1 > /sys/class/gpio/gpio11/value
+
+echo 12 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio12/direction
+echo 1 > /sys/class/gpio/gpio12/value
+
+echo 13 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio13/direction
+echo 1 > /sys/class/gpio/gpio13/value
+
+echo 14 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio14/direction
+echo 1 > /sys/class/gpio/gpio14/value
+
+echo 15 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio15/direction
+echo 1 > /sys/class/gpio/gpio15/value
+
+echo 16 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio16/direction
+echo 1 > /sys/class/gpio/gpio16/value
+
+echo 17 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio10/direction
+echo 1 > /sys/class/gpio/gpio10/value
+
+echo 18 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio18/direction
+echo 1 > /sys/class/gpio/gpio18/value
+
+echo 19 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio19/direction
+echo 1 > /sys/class/gpio/gpio19/value
+
+echo 20 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio20/direction
+echo 1 > /sys/class/gpio/gpio20/value
+
+echo 21 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio21/direction
+echo 1 > /sys/class/gpio/gpio21/value
+
+echo 22 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio22/direction
+echo 1 > /sys/class/gpio/gpio22/value
+
+echo 23 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio23/direction
+echo 1 > /sys/class/gpio/gpio23/value
+
+echo 24 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio24/direction
+echo 1 > /sys/class/gpio/gpio24/value
+
+echo 25 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio25/direction
+echo 1 > /sys/class/gpio/gpio25/value
