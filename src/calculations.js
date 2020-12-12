efe9d74d3fb4a97b15bbb014dfdeb96ed4600595
Anders <anders.aamodt@gmail.com>
2020-12-12 03:18:21 -0800

    fixed. garbage data got in db somehow

--- src/calculations.js
+++ src/calculations.js
@@ -65,7 +65,7 @@ function blankCard(
     taskId,
     color,
     deck,
-    name: name.hasOwnProperty('trim') ? name.trim() : name,
+    name: String(name).trim(),
     address: '',
     bolt11: '',
     book: {},
