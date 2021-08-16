2cde9b5155500099bffbedebe047a4732f937cc8
Anders <anders.aamodt@gmail.com>
2021-08-15 18:41:07 -0700

    undo

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -60,10 +60,6 @@ const AoGridRowObserver = observer(
       }
 
       const card = aoStore.hashMap.get(tId)
-      if (!card) {
-        console.log('missing card in grid: ', tId)
-        continue
-      }
       render.push(
         <AoDropZone
           taskId={tId}
@@ -143,10 +139,6 @@ const AoGridRow: Function = (props: {
     }
 
     const card = aoStore.hashMap.get(tId)
-    if (!card) {
-      console.log('missing card in grid row: ', tId)
-      continue
-    }
     render.push(
       <AoDropZone
         taskId={tId}
