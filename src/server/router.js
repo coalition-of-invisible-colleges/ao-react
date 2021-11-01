3dd23e71680b26a96112e5f3db7edf6e0e48f4d9
Anders <anders.aamodt@gmail.com>
2021-11-01 02:43:05 -0700

    lower maxage in cache?

--- src/server/router.js
+++ src/server/router.js
@@ -297,7 +297,7 @@ export default function applyRouter(app) {
     console.log('meme path is ', memePath)
     // res.contentType(memePath)
     res
-      .set('Cache-Control', 'public,max-age=31557600,immutable')
+      .set('Cache-Control', 'public,max-age=31536000,immutable')
       .sendFile(memePath)
   })
 
@@ -307,7 +307,7 @@ export default function applyRouter(app) {
       return meme.hash === req.params.memeHash
     })
     const memePath = path.join(config.memes.dir, meme.filename)
-    console.log('meme path is ', memePath)
+    console.log('meme path is ', memePath
     // res.contentType(memePath)
     res.download(memePath)
   })
