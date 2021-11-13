f9f95482eaf31146fe8c6b83b9d829da43fe506d
Zenen Treadwell <zenen52@gmail.com>
2021-11-13 13:17:57 -0800

    Use auth first, if available

--- src/server/auth.js
+++ src/server/auth.js
@@ -42,17 +42,28 @@ export function socketAuth(socket, data, callback) {
   callback(null, authorized)
 }
 
+// This function receives an authentication request and processes it
 export function serverAuth(req, res, next) {
+    console.log('headers', req.headers)
+    console.log('cookies', req.cookies)
+    // Firstly, it looks through the server's list of members to see if the username
+    // matches an active profile, and returns their secret if it does
   const { ownerId, secret } = getIdSecret(req.headers.name)
   let authorization
-  if (req.cookies.token) {
-    authorization = req.cookies.token
-  } else {
-    authorization = req.headers.authorization
-  }
-  if (secret && authorization && req.headers.session) {
+  
+    // If the user is actively providing a password, use that for authorization.
+    // Otherwise, attempt to use an available token in cookies
+  const authorization = req.headers.authorization || req.cookies.token
+
+  if (secret && req.headers.session) {
+      // If the user is in the list of members (secret)
+      // and has initiated a session (doesn't happen on auto-login), then proceed
+      // with the creation of a new session.
     let sessionKey = createHash(req.headers.session + secret)
     let token = hmacHex(req.headers.session, sessionKey)
+      
+      // Go through the same process to create the token and see if it matches the
+      // authorization provided
     if (token === authorization) {
       // client able to create the token, must have secret
       res.cookie('token', token, {
@@ -72,7 +83,8 @@ export function serverAuth(req, res, next) {
       res.status(401).end('unauthorized')
     }
   } else {
-    // otherwise we validate their authorization token in the header
+      // If they are not passing a session ID or a username, then we check their
+      // authorization against the list of active sessions and look for a match
     let authorized = false
     state.serverState.sessions.forEach(session => {
       if (session.token === authorization) {
