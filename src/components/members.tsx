fe366ba3eef71c3021ac38228100677aa5d6a62d
andersaamodt <anders.aamodt@gmail.com>
2022-03-31 10:50:14 -0700

    param

--- src/components/members.tsx
+++ src/components/members.tsx
@@ -190,7 +190,7 @@ export default class AoMembers extends React.Component<{}, State> {
       })
     } else if (this.state.sort === 'alphabetical') {
       members.sort((a, b) => {
-        return b.name ? b.name.localeCompare(a.name, { sensitivity: 'base' }) : 0
+        return b.name ? b.name.localeCompare(a.name, 'en', { sensitivity: 'base' }) : 0
       })
     }
 
