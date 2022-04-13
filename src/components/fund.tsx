b78c68a70b26160725065c5362b4503e7855664f
andersaamodt <anders.aamodt@gmail.com>
2022-04-12 23:50:51 -0700

    small update to show address

--- src/components/fund.tsx
+++ src/components/fund.tsx
@@ -13,46 +13,94 @@ interface Props {
   taskId: string
 }
 
-interface State {
-  qrCode?: boolean
-}
 
-@observer
-export default class AoFund extends React.Component<Props, State> {
-  constructor(props) {
-    super(props)
-    // makeObservable(this)
-    this.state = {}
-    this.requestQr = this.requestQr.bind(this)
+export default function AoFund (props: Props) {
+  const [tab, setTab] = React.useState(false)
+  const [text, setText] = React.useState('0')
+  const [valid, setValid] = React.useState(true)
+  
+  const goFirstTab = () => {
+    setTab(false)
   }
-
-  componentDidMount() {}
-
-  requestQr() {
-    api.requestBtcQr(this.props.taskId)
+  
+  const goOtherTab = () => {
+    setTab(true)
   }
-
-  render() {
-    const card = aoStore.hashMap.get(this.props.taskId)
-    if (!card) {
-      console.log('Attempt to access lightning QR code on mssing card.')
-      return null
+  
+  const requestBtcQr = () => {
+    api.requestBtcQr(props.taskId)
+  }
+  
+  const requestLightningInvoice = () => {
+    const amount = parseInt(text) 
+    api.requestLightningInvoice(props.taskId, amount)
+  }
+  
+  const onChange = (event) => {
+    setText(event.target.value)
+    if(isNaN(event.target.value)) {
+      setValid(false)
+    } else {
+      setValid(true)
     }
+  }
+
+  const handleFocus = (event) => event.target.select()
+
+  const card = aoStore.hashMap.get(props.taskId)
+  if (!card) {
+    console.log('Attempt to access funding tab on missing card.')
+    return null
+  }
+  
+  let contents
+  if(!tab) {
     if (card.address) {
-      return (
-        <div className="fund menu">
+      contents = (
+        <React.Fragment>
           <div>Send mainnet BTC here to fund this card:</div>
           <QRCode value={card.address} />
-        </div>
+          <div>{card.address}</div>
+        </React.Fragment>
       )
     } else {
-      return (
-        <div className="fund menu">
-          <div onClick={this.requestQr} className="action">
+      contents = (
+        <React.Fragment>
+          <div onClick={requestBtcQr} className="action">
             send BTC here
           </div>
-        </div>
+        </React.Fragment>
+      )
+    }
+  } else {
+    if (card.bolt11) {
+      contents = (
+        <React.Fragment>
+          <div>Send lightning network BTC here to fund this card:</div>
+          <QRCode value={card.bolt11} />
+          <textarea readOnly={true} value={card.bolt11} />
+        </React.Fragment>
+      )
+    } else {
+      contents = (
+        <React.Fragment>
+          <label htmlFor='invoiceAmount'>Amount:</label>
+          <input type='text' disabled={!valid} size={3} id='invoiceAmount' value={text} onChange={onChange} autoFocus onFocus={handleFocus} />
+          <button onClick={requestLightningInvoice} className="action">
+            Generate Invoice
+          </button>
+        </React.Fragment>
       )
     }
   }
+  
+  return (
+    <div className="fund menu">
+      <div className='toolbar'>
+        <div onClick={goFirstTab} className={'action' + (!tab ? ' selected' : '')}>BTC</div>
+        <div onClick={goOtherTab} className={'action'+ (tab ? ' selected' : '')}>Lightning</div>
+      </div>
+      {contents}
+    </div>
+  )
 }
